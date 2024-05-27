6d2c1bd8ebb5f38511642baaae4b9c0b2294f7ce
user <user@Ubuntu1>
2024-05-27 09:48:41 +0200

    Mi Primer Commit

---
+++ src/juanma/Saludo.java
@@ -0,0 +1,11 @@
+package juanma;
+
+public class Saludo {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hola esto es la version 1 del programa de Saludo, mi nombre es Juanma");
+
+	}
+
+}
